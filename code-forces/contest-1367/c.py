49d72470c3bc333b4ea403e792839a48aa5a4125
Cory <cory.forsyth@gmail.com>
2020-06-23 12:03:06 -0400

    1368 and 1367 c

--- code-forces/contest-1367/c.py
+++ code-forces/contest-1367/c.py
@@ -1,58 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[13]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
 # In[30]:
-
-
-def memoize(f):
-    memo = {}
-    def helper(s,k):
-        if (s,k) not in memo:            
-            memo[(s,k)] = f(s,k)
-        return memo[(s,k)]
-    return helper
-    
-@memoize
-def avail_inner_slots(size, k):
-    if size >= 2*k + 1:
-        return 1 + avail_inner_slots(size - k - 1, k)
-    else:
-        return 0
-    
-for size in range(5,8):
-    assert(avail_inner_slots(size,2) == 1)
-for size in range(8,11):
-    assert(avail_inner_slots(size,2) == 2)
-
-assert(avail_inner_slots(11,3) == 2)
 
 
 # In[31]:
 
 
-# unbounded on one side
-def avail_outer_slots(size, k):
-    return avail_inner_slots(size+k,k)
-    
-# unbounded on both sides
-def avail_double_outer_slots(size, k):
-    return avail_inner_slots(size+2*k,k)
-
-assert(avail_double_outer_slots(6,2) == 2)
-assert(avail_outer_slots(3,1) == 1)
-
-
-# In[32]:
-
-
 def solve(s, k):
-    sizes = [len(_s) for _s in s.split('1')]
-    if len(sizes) == 1:
-        assert(sizes[0] == len(s))
-        return avail_double_outer_slots(len(s), k)
-    else:
-        return avail_outer_slots(sizes[0], k) +                sum([avail_inner_slots(sizes[i], k) for i in range(1,len(sizes) - 1)]) +                avail_outer_slots(sizes[-1], k)
+    s = '1'+'0'*k + s + '0'*k + '1'
+    blocks = s.split('1')
+#     print(blocks)
+    total = 0
+    for block in blocks:
+        size = len(block)
+        if size == 0:
+            continue
+        else:
+            #             print(block,size,k,(size-k)//(k+1))
+            total += max(0, (size-k)//(k+1))
+    return total
 
+
+# print(solve('100010', 1))
 assert(solve('100010', 1) == 1)
 assert(solve('000000', 2) == 2)
 assert(solve('10101', 1) == 0)
@@ -66,10 +42,13 @@ assert(solve('0', 1) == 1)
 
 def main():
     T = int(input())
-    for _ in range(T):
-        k = int(input().split(' ')[1])
+    for i in range(T):
+        (l, k) = map(int, input().split(' '))
         s = input()
-        print(solve(s,k))
+        try:
+            print(solve(s, k))
+        except Exception as e:
+            print("exception test %s, k=%s: %s" % (i, k, str(e)))
 
 
 # In[ ]:
@@ -77,3 +56,5 @@ def main():
 
 main()
 
+
+# In[ ]:
--- code-forces/contest-1368/c.py
+++ code-forces/contest-1368/c.py
@@ -6,6 +6,7 @@
 
 from math import sqrt
 
+
 def solve(n):
     # 2*x*(x+1) = n for some x
     # x**2+x-(n/2) = 0
@@ -16,56 +17,56 @@ def solve(n):
     k = 1
     cur_n = 2 * k * (k+1)
     cells = {(0, 1),
-     (0, 2),
-     (1, 0),
-     (1, 1),
-     (1, 2),
-     (1, 3),
-     (2, 0),
-     (2, 1),
-     (2, 2),
-     (2, 3),
-     (3, 1),
-     (3, 2)
-    }
+             (0, 2),
+             (1, 0),
+             (1, 1),
+             (1, 2),
+             (1, 3),
+             (2, 0),
+             (2, 1),
+             (2, 2),
+             (2, 3),
+             (3, 1),
+             (3, 2)
+             }
     edge = {(0, 1),
-     (0, 2),
-     (1, 0),
-     (1, 1),
-     (1, 2),
-     (1, 3),
-     (2, 0),
-     (2, 1),
-     (2, 2),
-     (2, 3),
-     (3, 1),
-     (3, 2)
-    }
+            (0, 2),
+            (1, 0),
+            (1, 1),
+            (1, 2),
+            (1, 3),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+            (2, 3),
+            (3, 1),
+            (3, 2)
+            }
     while cur_n < n:
         # expand edge
         next_edge = set()
-        for (x,y) in edge:
-#             print(x,y)
-            if not ((x-1,y) in cells):
-                next_edge.add((x-1,y))
-            if not ((x+1,y) in cells):
-                next_edge.add((x+1,y))
-            if not ((x,y-1) in cells):
-                next_edge.add((x,y-1))
-            if not ((x,y+1) in cells):
-                next_edge.add((x,y+1))
+        for (x, y) in edge:
+            #             print(x,y)
+            if not ((x-1, y) in cells):
+                next_edge.add((x-1, y))
+            if not ((x+1, y) in cells):
+                next_edge.add((x+1, y))
+            if not ((x, y-1) in cells):
+                next_edge.add((x, y-1))
+            if not ((x, y+1) in cells):
+                next_edge.add((x, y+1))
         edge = next_edge
         # update n
         k += 1
         cur_n = 2 * k * (k+1)
         # update cells
-        for (x,y) in edge:
-            cells.add((x,y))
+        for (x, y) in edge:
+            cells.add((x, y))
     assert(cur_n == n)
 #     print(k,cur_n,n)
     print(len(cells))
-    for (x,y) in cells:
-        print("%d %d" % (x,y))
+    for (x, y) in cells:
+        print("%d %d" % (x, y))
 
 # solve(4)
 
@@ -75,7 +76,10 @@ def solve(n):
 
 def main():
     n = int(input())
-    solve(n)
+    try:
+        solve(n)
+    except Exception as e:
+        print("could not solve %s" % (str(e)))
 
 
 # In[8]:
@@ -85,7 +89,4 @@ main()
 
 
 # In[ ]:
-
-
-
 
