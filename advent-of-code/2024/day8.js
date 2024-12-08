9aeb085d3a0213da267071b0acadd0023fa5169c
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-08 09:03:20 +0100

    advent of code 2024 day 8 p2

--- advent-of-code/2024/day8.js
+++ advent-of-code/2024/day8.js
@@ -16,29 +16,48 @@ for (let y = 0; y < grid.length; y++) {
 }
 let pairs = (arr) =>
   arr.flatMap((v1, idx) => arr.slice(idx + 1).map((v2) => [v1, v2]));
-let getAntinodes = ([[x1, y1], [x2, y2]]) => {
+let getAntinodes = ([[x1, y1], [x2, y2]], part1 = true) => {
   let dx = x2 - x1;
   let dy = y2 - y1;
-  return [
-    [x1 + 2 * dx, y1 + 2 * dy],
-    [x2 - 2 * dx, y2 - 2 * dy],
-  ];
-};
-let antinodes = [];
-for (let node of Object.keys(nodes)) {
-  let coords = nodes[node].map((v) => v.split(",").map(toInt));
-  // console.log("node", node, coords);
-  for (let pair of pairs(coords)) {
-    // console.log(pair);
-    for (let antinode of getAntinodes(pair)) {
-      // console.log(antinode);
-      if (inGrid(antinode)) {
+  if (part1) {
+    return [
+      [x1 + 2 * dx, y1 + 2 * dy],
+      [x2 - 2 * dx, y2 - 2 * dy],
+    ].filter(inGrid);
+  } else {
+    let antinodes = [];
+    // +
+    {
+      let antinode = [x1, y1];
+      while (inGrid(antinode)) {
         antinodes.push(antinode);
+        let [x, y] = antinode;
+        antinode = [x + dx, y + dy];
       }
     }
+    // -
+    {
+      let antinode = [x2, y2];
+      while (inGrid(antinode)) {
+        antinodes.push(antinode);
+        let [x, y] = antinode;
+        antinode = [x - dx, y - dy];
+      }
+    }
+    return antinodes.filter(inGrid);
+  }
+};
+let antinodes1 = [];
+let antinodes2 = [];
+for (let node of Object.keys(nodes)) {
+  let coords = nodes[node].map((v) => v.split(",").map(toInt));
+  for (let pair of pairs(coords)) {
+    antinodes1.push(...getAntinodes(pair, true));
+    antinodes2.push(...getAntinodes(pair, false));
   }
 }
-let uniqueAntinodes = new Set(antinodes.map((pos) => pos.join(",")));
-let p1 = uniqueAntinodes.size;
+let uniq = (nodes) => new Set(nodes.map((pos) => pos.join(",")));
+let p1 = uniq(antinodes1).size;
+let p2 = uniq(antinodes2).size;
 
-console.log({ p1 });
+console.log({ p1, p2 });
