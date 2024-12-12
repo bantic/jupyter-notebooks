3ffc2a255e6878d8349444e53db3529d7e2d325b
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-12 12:39:47 +0100

    advent of code 2024 day 12 part 2

--- advent-of-code/2024/day12.js
+++ advent-of-code/2024/day12.js
@@ -2,39 +2,65 @@ fs = require("node:fs");
 assert = require("node:assert");
 data = fs.readFileSync("./data/day12-test.txt", "utf8").trim();
 data = fs.readFileSync("./data/day12.txt", "utf8").trim();
-grid = data.split('\n').map(l => l.split(''));
-OUT = Symbol('out');
-get = ([x,y]) => grid[y]?.[x] ?? OUT;
-dirs = [[-1,0],[1,0],[0,1],[0,-1]];
-move = ([x,y],[dx,dy]) => [x+dx,y+dy];
-toKey = pos => pos.join(',');
-sum = arr => arr.reduce((acc,m) => acc+m,0);
-let inGrid = ([x,y]) => get([x,y]) !== OUT;
-grid[Symbol.iterator] = function*() {
-  let minY = 0, maxY = grid.length;
-  let minX = 0, maxX = grid[0].length;
+// data = `AAAA
+// BBCD
+// BBCC
+// EEEC`.trim();
+// data = `EEEEE
+// EXXXX
+// EEEEE
+// EXXXX
+// EEEEE`.trim();
+// data = `AAAAAA
+// AAABBA
+// AAABBA
+// ABBAAA
+// ABBAAA
+// AAAAAA`.trim();
+grid = data.split("\n").map((l) => l.split(""));
+OUT = Symbol("out");
+get = ([x, y]) => grid[y]?.[x] ?? OUT;
+namedDirs = {
+  up: [0, -1],
+  down: [0, 1],
+  left: [-1, 0],
+  right: [1, 0],
+};
+ydirs = [namedDirs.up, namedDirs.down];
+xdirs = [namedDirs.left, namedDirs.right];
+dirs = Object.values(namedDirs);
+move = ([x, y], [dx, dy]) => [x + dx, y + dy];
+toKey = (pos) => pos.join(",");
+sum = (arr) => arr.reduce((acc, m) => acc + m, 0);
+countUniq = (arr) => new Set(arr).size;
+let inGrid = ([x, y]) => get([x, y]) !== OUT;
+grid[Symbol.iterator] = function* () {
+  let minY = 0,
+    maxY = grid.length;
+  let minX = 0,
+    maxX = grid[0].length;
   for (let x = minX; x < maxX; x++) {
     for (let y = minY; y < maxY; y++) {
-      yield [x,y];
+      yield [x, y];
     }
   }
-}
+};
 
-segment = (grid,dirs) => {
+segment = (grid, dirs) => {
   let regions = [];
   let seen = new Set();
   for (let pos of grid) {
     if (seen.has(toKey(pos))) continue;
-    let region = fill(pos,dirs);
+    let region = fill(pos, dirs);
     for (let pos of region) {
       seen.add(toKey(pos));
     }
     regions.push(region);
   }
   return regions;
-}
+};
 
-fill = (pos,dirs) => {
+fill = (pos, dirs) => {
   let v = get(pos);
   let seen = new Set();
   let region = [];
@@ -44,27 +70,88 @@ fill = (pos,dirs) => {
     if (seen.has(toKey(pos))) continue;
     seen.add(toKey(pos));
     region.push(pos);
-    let moves = dirs.map(d => move(pos,d)).filter(inGrid).filter(p => get(p) === v);
+    let moves = dirs
+      .map((d) => move(pos, d))
+      .filter(inGrid)
+      .filter((p) => get(p) === v);
     stack = [...stack, ...moves];
   }
   return region;
-}
-perimeter = (region,dirs) => {
+};
+perimeter = (region, dirs) => {
   let perim = 0;
   for (let pos of region) {
     let v = get(pos);
-    let moves = dirs.map(d => move(pos,d)).filter(p => get(p) === v);
+    let moves = dirs.map((d) => move(pos, d)).filter((p) => get(p) === v);
     perim += dirs.length - moves.length;
   }
   return perim;
+};
+adjoins = ([x1, y1], [x2, y2]) => {
+  let dx = Math.abs(x2-x1);
+  let dy = Math.abs(y2-y1);
+  return (dx+dy) === 1;
+};
+sortLtoR = (region) => {
+  return region.toSorted(([x1, y1], [x2, y2]) => {
+    return x2 > x1 ? -1 : x2 < x1 ? 1 : y2 > y1 ? -1 : y2 < y1 ? 1 : 0;
+  });
+};
+countSides = (region) => {
+  let tops = [];
+  let bottoms = [];
+  let lefts = [];
+  let rights = [];
+  let {up,down,left,right} = namedDirs;
+  getDir = (pos,dir) => get(move(pos,dir));
+  for (let pos of region) {
+    let v = get(pos);
+    if (getDir(pos,up) !== v) {
+      tops.push(move(pos,up));
+    }
+    if (getDir(pos,down) !== v) {
+      bottoms.push(move(pos,down));
+    }
+    if (getDir(pos,right) !== v) {
+      rights.push(move(pos,right));
+    }
+    if (getDir(pos,left) !== v) {
+      lefts.push(move(pos,left));
+    }
+  }
+  return sum([tops,bottoms,lefts,rights].map(pos_s => countUniqueRegions(pos_s)));
+};
+countUniqueRegions = (pos_s) => {
+  let seen = [];
+  let stack = sortLtoR(pos_s);
+  let count = 0;
+  while (stack.length) {
+    let pos = stack.shift();
+    if (!seen.some(p => adjoins(p,pos))) {
+      count += 1;
+    }
+    seen.push(pos);
+  }
+  return count;
 }
 
-let regions = segment(grid,dirs);
+let regions = segment(grid, dirs);
 
-let p1 = sum(regions.map(r => {
-  let area = r.length;
-  let perim = perimeter(r,dirs);
-  return area * perim;
-}));
+let p1 = sum(
+  regions.map((r) => {
+    let area = r.length;
+    let perim = perimeter(r, dirs);
+    return area * perim;
+  })
+);
 // 203874566 too high
-console.log({p1})
+console.log({ p1 });
+let p2 = sum(regions.map(r => {
+  // console.log(get(r[0]));
+  let area = r.length;
+  let s = countSides(r);
+  // console.log(get(r[0]),area,s);
+  return area * s;
+}));
+// // // 4251040 too high
+console.log({p2})
