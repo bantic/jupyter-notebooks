ff6db50913451a520e2be5e70e8a1290b49e2b71
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-14 19:21:32 +0100

    advent of code 2024 day 14 p2

--- advent-of-code/2024/day14.js
+++ advent-of-code/2024/day14.js
@@ -32,21 +32,44 @@ toQ = (pos) => {
   if ([qY,qX].includes(NONE)) { return 'none'; }
   return [qX,qY].join(',');
 }
-bots = data.split('\n').map(toInts);
+parse = () => data.split('\n').map(toInts);
+bots = parse()
 
-console.log(bots);
-// b = bots[10]
-// for (let i = 0; i < 6; i++) {
-//   console.log(i,JSON.stringify(b));
-//   b = moveB(b);
-// }
+print = (bots, width, height) => {
+  let grid = [];
+  for (let y = 0; y < height; y++) {
+    let line = '.'.repeat(width).split('');
+    grid.push(line);
+  }
+  for (let [x,y] of bots) {
+    let v = grid[y][x];
+    if (v === '.') {
+      v = 1;
+    } else {
+      v = parseInt(v) + 1;
+    }
+    grid[y][x] = v;
+  }
+  console.log(grid.map(l => l.join('')).join('\n'));
+}
 
 for (let i = 0; i < 100; i++) {
   bots = bots.map(moveB);
 }
-console.log(bots);
 byQ = {};
 let qs = bots.map(b => toQ(b)).filter(q => q !== 'none');
 let counts = counter(qs);
 let p1 = prod(Object.values(counts));
 console.log({p1});
+
+bots = parse();
+// Had a hunch that the xmas tree would only occur when no bots are on top of each other, and
+// that turned out to be true
+for (let i = 0; i < 15000; i++) {
+  if (Object.values(counter(bots.map(([x,y]) => `${x},${y}`))).every(v => v === 1)) {
+    console.log({p2:i});
+    print(bots,width,height);
+    break;
+  }
+  bots = bots.map(moveB);
+}
