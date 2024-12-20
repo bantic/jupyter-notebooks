16458283ac9598cfa6b1904f0afa5a5ba7736b30
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-20 11:34:35 +0100

    advent of code 2024 day 20 p1

---
+++ advent-of-code/2024/day20.mjs
@@ -0,0 +1,120 @@
+import fs from 'node:fs';
+import assert from 'node:assert';
+let data = fs.readFileSync("./data/day20.txt", "utf8").trim();
+let testdata = fs.readFileSync("./data/day20-test.txt", "utf8").trim();
+
+// data = testdata;
+
+let FREE = '.';
+let WALL = '#';
+let OUT = Symbol();
+let E = [0,1];
+let W = [0,-1];
+let S = [1,0];
+let N = [-1,0];
+let DIRS = [N,E,S,W];
+
+let parse = data => {
+  let grid = [];
+  let start, end;
+  data.split('\n').map((line,y) => {
+    grid[y] = line.split('');
+    if (line.includes('S')) start = [line.indexOf('S'), y];
+    if (line.includes('E')) end = [line.indexOf('E'), y];
+  });
+  grid.get = ([x,y]) => grid[y]?.[x] ?? OUT;
+  grid[start[1]][start[0]] = FREE;
+  grid[end[1]][end[0]] = FREE;
+  return {grid,start,end};
+}
+
+let vecadd = (v1,v2) => {
+  assert(v1.length === v2.length);
+  return v1.map((_,idx) => v1[idx]+v2[idx]);
+};
+let veceq = (v1,v2) => v1.length === v2.length && v1.every((v,idx) => v === v2[idx]);
+let vecmul = (v1,mul) => v1.map(v => v*mul);
+let sum = arr => arr.reduce((acc,m) => acc+m,0);
+let manhattanDistance = (v1,v2) => {
+  assert(v1.length === v2.length);
+  return sum(vecadd(vecmul(v1,-1),v2).map(Math.abs));
+}
+let md = manhattanDistance;
+assert(veceq([1,2],[1,2]));
+assert(!veceq([1,2],[2,2]));
+assert(veceq(vecadd([0,1],[1,5]), [1,6]));
+let n4 = p => DIRS.map(d => vecadd(d,p));
+let hash = JSON.stringify;
+let cheats8 = p => {
+  return [
+    [N,N],
+    [N,W],
+    [W,W],
+    [W,S],
+    [S,S],
+    [S,E],
+    [E,E],
+    [E,N]
+  ].map(([d1,d2]) => {
+    let p1 = vecadd(d1,p);
+    let p2 = vecadd(d2,p1);
+    return [p1,p2]
+  });
+}
+
+let traverse = (grid,start,end) => {
+  let path = [];
+  let costs = new Map();
+  let pos = start;
+  let steps = 0;
+  while (true) {
+    costs.set(hash(pos),steps);
+    path.push(pos);
+
+    if (veceq(pos,end)) break;
+
+    let next = n4(pos)
+      .filter(p => grid.get(p) === FREE)
+      .filter(p => !costs.has(hash(p)));
+    assert(next.length === 1);
+    pos = next[0];
+    steps += 1;
+  }
+  return [path,costs];
+}
+
+let solve = data => {
+  let {grid,start,end} = parse(data);
+  let [path,costs] = traverse(grid,start,end);
+  let cheatCosts = new Map();
+
+  for (let p of path) {
+    let pCost = costs.get(hash(p));
+
+    let valid = cheats8(p).filter(([p1,p2]) => grid.get(p1) === WALL && grid.get(p2) === FREE);
+    let newValid = valid.filter(cheat => !cheatCosts.has(hash(cheat)));
+
+    for (let cheat of newValid) {
+      assert(!veceq(cheat[0],end)); // shouldn't be possible
+
+      let oldCost = costs.get(hash(cheat[1]));
+      let newCost = pCost + 2;
+      let save = oldCost - newCost;
+      cheatCosts.set(hash(cheat), save);
+    }
+  }
+
+  let p1 = 0;
+  let posCheats = Array.from(cheatCosts.entries()).filter(([cheat,savings]) => savings > 0);
+  debugger;
+  for (let [cheat,savings] of cheatCosts) {
+    if (savings >= 100) {
+      p1 += 1;
+    }
+  }
+  return p1;
+}
+
+// 2002 too high
+let p1 = solve(data);
+console.log({p1});
