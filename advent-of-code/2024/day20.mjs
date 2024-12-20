6b99ff0569dbfb6db2bdab8b6de40cb2b6fc8e9a
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-20 14:04:56 +0100

    advent of code 2024 day 20 refactor

--- advent-of-code/2024/day20.mjs
+++ advent-of-code/2024/day20.mjs
@@ -6,7 +6,6 @@ let testdata = fs.readFileSync("./data/day20-test.txt", "utf8").trim();
 // data = testdata;
 
 let FREE = '.';
-let WALL = '#';
 let OUT = Symbol();
 let E = [0,1];
 let W = [0,-1];
@@ -36,7 +35,6 @@ let veceq = (v1,v2) => v1.length === v2.length && v1.every((v,idx) => v === v2[i
 let vecmul = (v1,mul) => v1.map(v => v*mul);
 let sum = arr => arr.reduce((acc,m) => acc+m,0);
 let manhattanDistance = (v1,v2) => {
-  if (v1.length !== v2.length) debugger;
   assert(v1.length === v2.length);
   return sum(vecadd(vecmul(v1,-1),v2).map(Math.abs));
 }
@@ -46,22 +44,6 @@ assert(!veceq([1,2],[2,2]));
 assert(veceq(vecadd([0,1],[1,5]), [1,6]));
 let n4 = p => DIRS.map(d => vecadd(d,p));
 let hash = JSON.stringify;
-let cheats8 = p => {
-  return [
-    [N,N],
-    [N,W],
-    [W,W],
-    [W,S],
-    [S,S],
-    [S,E],
-    [E,E],
-    [E,N]
-  ].map(([d1,d2]) => {
-    let p1 = vecadd(d1,p);
-    let p2 = vecadd(d2,p1);
-    return [p1,p2]
-  });
-}
 
 let traverse = (grid,start,end) => {
   let path = [];
@@ -84,45 +66,14 @@ let traverse = (grid,start,end) => {
   return [path,costs];
 }
 
-let solve = data => {
+let solve = (data, {p1,p2}) => {
   let {grid,start,end} = parse(data);
   let [path,costs] = traverse(grid,start,end);
-  let cheatCosts = new Map();
-
-  for (let p of path) {
-    let pCost = costs.get(hash(p));
-
-    let valid = cheats8(p).filter(([p1,p2]) => grid.get(p1) === WALL && grid.get(p2) === FREE);
-    let newValid = valid.filter(cheat => !cheatCosts.has(hash(cheat)));
-
-    for (let cheat of newValid) {
-      assert(!veceq(cheat[0],end)); // shouldn't be possible
-
-      let oldCost = costs.get(hash(cheat[1]));
-      let newCost = pCost + 2;
-      let save = oldCost - newCost;
-      cheatCosts.set(hash(cheat), save);
-    }
-  }
-
-  return Array.from(cheatCosts.entries()).filter(([cheat,savings]) => savings >= 100).length;
-}
-
-// let p1 = solve(data);
-// console.log({p1});
-
-let solve2 = data => {
-  let {grid,start,end} = parse(data);
-  let [path,costs] = traverse(grid,start,end);
-  let maxlen = 20;
+  let maxlen = p1 ? 2 : 20;
 
   let cheatCosts = new Map();
 
-  let _idx = 0;
   for (let i = 0; i < path.length; i++) {
-    _idx++;
-    if (_idx % 100 === 0) { console.log(_idx,path.length); }
-
     let p = path[i];
     let pCost = costs.get(hash(p));
     for (let j = i+1; j < path.length; j++) {
@@ -136,11 +87,10 @@ let solve2 = data => {
     }
   }
 
-  debugger;
-  let p2 = Array.from(cheatCosts.entries()).filter(([cheat,savings]) => savings >= 100).length;
-  return p2;
+  return Array.from(cheatCosts.entries()).filter(([cheat,savings]) => savings >= 100).length;
 }
 
-
-let p2 = solve2(data);
-console.log({p2});
+console.log('Takes 5-10s to run...');
+let p1 = solve(data, {p1:true});
+let p2 = solve(data, {p2:true});
+console.log({p1, p2});
