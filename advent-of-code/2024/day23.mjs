3f0c6c8513e7d14390414eeda82425de3a05cc05
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-23 11:18:20 +0100

    advent of code 2024 day 23 p1

---
+++ advent-of-code/2024/day23.mjs
@@ -0,0 +1,123 @@
+import fs from 'node:fs';
+import assert from 'node:assert';
+let data = fs.readFileSync("./data/day23.txt", "utf8").trim();
+
+let testdata = `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn`.trim();
+
+class Graph {
+  nodes = new Set();
+  edges = {};
+
+  addEdge(a,b) {
+    this.nodes.add(a);
+    this.nodes.add(b);
+    this.edges[a] ??= new Set();
+    this.edges[b] ??= new Set();
+    this.edges[a].add(b);
+    this.edges[b].add(a);
+  }
+}
+
+let parse = data => {
+  let g = new Graph();
+  let lines = data.split('\n');
+  for (let line of lines) {
+    let [a,b] = line.split('-');
+    g.addEdge(a,b);
+  }
+  return g;
+}
+
+let combinations2 = (arr) => {
+  let out = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      out.push(
+        [arr[i],arr[j]]
+      );
+    }
+  }
+  return out;
+}
+
+let combinations3 = (arr) => {
+  let out = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      for (let k = j+1; k < arr.length; k++) {
+        out.push(
+          [arr[i],arr[j],arr[k]]
+        );
+      }
+    }
+  }
+  return out;
+}
+
+// console.log(combinations2([1,2,3,4]));
+
+let solve = data => {
+  let g = parse(data);
+  let is3Cycle = ([a,b,c]) => {
+    return g.edges[a]?.has(b) && g.edges[a]?.has(c) && g.edges[b]?.has(c);
+  };
+  let is4Cycle = ([a,b,c,d]) => {
+    return g.edges[a]?.has(b) &&
+           g.edges[a]?.has(c) &&
+           g.edges[a]?.has(d) &&
+           g.edges[b]?.has(c) &&
+           g.edges[b]?.has(d) &&
+           g.edges[c]?.has(d);
+  };
+  let get3Cycles = n => {
+    let combos = combinations2(Array.from(g.edges[n]));
+    return combos.filter(c => is3Cycle([n,...c])).map(([a,b]) => [n,a,b].sort());
+  };
+  let get4Cycles = n => {
+    let combos = combinations3(Array.from(g.edges[n]));
+    return combos.filter(c => is4Cycle([n,...c])).map(([a,b,c]) => [n,a,b,c].sort());
+  };
+
+  let all3Cycles = Object.keys(g.edges).flatMap(n => get3Cycles(n));
+  let all4Cycles = Object.keys(g.edges).flatMap(n => get4Cycles(n));
+  let uniq4Cycles = Array.from(new Set(all4Cycles.map(c => JSON.stringify(c)))).map(v => JSON.parse(v));
+  let uniq3Cycles = Array.from(new Set(all3Cycles.map(c => JSON.stringify(c)))).map(v => JSON.parse(v));
+
+  let uniqT3Cycles = uniq3Cycles.filter(arr => arr.some(v => v.startsWith('t')));
+  return uniqT3Cycles.length;
+}
+
+let p1 = solve(data);
+// 2466 too high
+console.log({p1});
