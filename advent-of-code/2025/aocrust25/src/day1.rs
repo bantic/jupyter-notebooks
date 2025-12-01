cdc9d52a40a56db21d7cdec4cc70a498982c45bb
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-01 17:24:09 +0100

    aoc 2025 day 1 p2

--- advent-of-code/2025/aocrust25/src/day1.rs
+++ advent-of-code/2025/aocrust25/src/day1.rs
@@ -2,28 +2,35 @@ use crate::utils::fs;
 
 pub fn run() {
     let input = fs::read(1).expect("failed to read day 1");
-    let p1 = p1(&input);
-    println!("p1 {p1}");
-}
+    let mut p1 = 0;
+    let mut p2 = 0;
 
-fn p1(input: &str) -> i16 {
-    let res = parse(input);
-
-    let mut hits = 0;
-    let mut start = 50;
-    for (dir, amt) in res {
-        start = turn(start, &dir, amt);
-        if start == 0 {
-            hits += 1;
+    let mut pos = 50;
+    for (dir, amt) in parse(&input) {
+        let d = dir as i16;
+        for _ in 0..amt {
+            pos += d;
+            if pos == 100 {
+                pos = 0;
+            }
+            if pos == -1 {
+                pos = 99;
+            }
+            if pos == 0 {
+                p2 += 1;
+            }
+        }
+        if pos == 0 {
+            p1 += 1;
         }
     }
-    hits
+    println!("p1 {p1}");
+    println!("p2 {p2}");
 }
 
-#[derive(Debug)]
 enum Dir {
-    L,
-    R,
+    L = -1,
+    R = 1,
 }
 
 fn parse(s: &str) -> Vec<(Dir, i16)> {
@@ -32,61 +39,9 @@ fn parse(s: &str) -> Vec<(Dir, i16)> {
     let mut out = vec![];
 
     for line in s.lines() {
-        let d: Dir = if line.starts_with('R') { R } else { L };
+        let d = if line.starts_with('R') { R } else { L };
         let amt: i16 = line[1..].parse().expect("failed to parse");
         out.push((d, amt));
     }
     out
-}
-
-fn turn(start: i16, d: &Dir, amt: i16) -> i16 {
-    let mut res = match d {
-        Dir::L => start - amt,
-        Dir::R => start + amt,
-    };
-    while res < 100 {
-        res += 100;
-    }
-    while res >= 100 {
-        res -= 100;
-    }
-    res
-}
-
-fn turn2(start: i16, d: &Dir, amt: i16) -> (i16, i8) {
-    let mut hits = 0;
-    let mut res = match d {
-        Dir::L => start - amt,
-        Dir::R => start + amt,
-    };
-    while res < 100 {
-        hits += 1;
-        res += 100;
-    }
-    while res >= 100 {
-        hits += 1;
-        res -= 100;
-    }
-    (res, hits)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Dir::*;
-    use super::*;
-
-    #[test]
-    fn mod_add() {
-        assert_eq!(turn(95, &R, 10), 5);
-        assert_eq!(turn(95, &R, 5), 0);
-        assert_eq!(turn(95, &R, 5 + 100 + 3), 3);
-    }
-
-    #[test]
-    fn mod_sub() {
-        assert_eq!(turn(5, &L, 10), 95);
-        assert_eq!(turn(5, &L, 5), 0);
-        assert_eq!(turn(5, &L, 5 + 100 + 3), 97);
-        assert_eq!(turn(50, &L, 68), 82);
-    }
 }
