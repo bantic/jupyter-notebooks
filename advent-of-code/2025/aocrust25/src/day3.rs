789cf861d713b696d1af9e38cb5252bdcdc853a1
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-03 21:51:23 +0100

    aoc 2025 day 3 refactor

--- advent-of-code/2025/aocrust25/src/day3.rs
+++ advent-of-code/2025/aocrust25/src/day3.rs
@@ -1,74 +1,31 @@
 use crate::utils::fs::read;
 
+const PART_1_LEN: i64 = 2;
+const PART_2_LEN: i64 = 12;
+
 pub fn run() {
     println!("day 3");
     let data = read(3).expect("failed to get data for day 3");
-    let p1 = solve(&data);
+    let p1 = solve(&data, PART_1_LEN);
     println!("p1 {p1}");
-    let p2 = solve2(&data);
+    let p2 = solve(&data, PART_2_LEN);
     println!("p2 {p2}");
 }
 
-fn solve2(inp: &str) -> i64 {
-    inp.lines().map(solve_line_2).sum()
+fn solve(inp: &str, max_len: i64) -> i64 {
+    inp.lines().map(|l| solve_line(l, max_len)).sum()
 }
 
-const PART_2_LEN: i64 = 12;
-
-fn solve_line_2(inp: &str) -> i64 {
+fn solve_line(inp: &str, max_len: i64) -> i64 {
     let digits = inp
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i64)
         .collect::<Vec<i64>>();
     let mut on_digits: Vec<Option<i64>> = vec![None; digits.len()];
     let mut on_count = 0;
-    // let on_count = || {
-    //     let mut count = 0;
-    //     for i in &on_digits {
-    //         if i.is_some() {
-    //             count += 1;
-    //         }
-    //     }
-    //     count
-    // };
 
-    fn rightmost_range(on_digits: &[Option<i64>]) -> (usize, usize) {
-        dbg!("RIGHTMOST RANGE", on_digits);
-
-        let end_idx = on_digits.iter().rposition(|d| d.is_none()).unwrap();
-        dbg!(end_idx);
-        let mut start_idx = None;
-        for i in (0..end_idx).rev() {
-            if on_digits[i].is_some() {
-                dbg!("on_digits[i].is_some", i);
-                start_idx = Some(i + 1);
-                break;
-            }
-        }
-        (start_idx.unwrap_or(0), end_idx)
-        // let mut start_idx = None;
-        // let mut end_idx = None;
-        // let len = on_digits.len();
-        // for idx in (0..len).rev() {
-        //     if end_idx.is_none() {
-        //         if on_digits[idx].is_none() {
-        //             end_idx = Some(idx);
-        //         }
-        //     } else if on_digits[idx].is_some() {
-        //         start_idx = Some(idx + 1);
-        //     }
-        // }
-
-        // if start_idx.is_none() {
-        //     start_idx = Some(0);
-        // }
-
-        // (start_idx.unwrap(), end_idx.unwrap())
-    }
-
-    while on_count < PART_2_LEN {
+    while on_count < max_len {
         let (start_idx, end_idx) = rightmost_range(&on_digits);
-        dbg!(on_count, "next range", start_idx, end_idx);
 
         let mut max = 0;
         let mut max_idx = 0;
@@ -79,54 +36,44 @@ fn solve_line_2(inp: &str) -> i64 {
             if v > max {
                 max = v;
                 max_idx = i;
-                dbg!("new max", max, max_idx);
             }
         }
 
-        dbg!("found", max, max_idx);
         on_digits[max_idx] = Some(max);
         on_count += 1;
     }
 
-    dbg!(&on_digits);
     on_digits
         .iter()
-        .filter(|&d| d.is_some())
-        .map(|d| d.unwrap().to_string())
+        .filter_map(|&d| d.map(|d| d.to_string()))
         .collect::<String>()
         .parse()
         .unwrap()
 }
 
-fn solve(inp: &str) -> i64 {
-    inp.lines().map(solve_line).sum()
-}
-
-fn solve_line(inp: &str) -> i64 {
-    let digits = inp
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as i64)
-        .collect::<Vec<i64>>();
-
-    let first = digits.iter().take(digits.len() - 1).max().unwrap();
-    let first_idx = digits.iter().position(|d| d == first).unwrap();
-    let second = digits.iter().skip(first_idx + 1).max().unwrap();
-
-    format!("{first}{second}").parse().unwrap()
+fn rightmost_range(on_digits: &[Option<i64>]) -> (usize, usize) {
+    let end_idx = on_digits.iter().rposition(|d| d.is_none()).unwrap();
+    let mut start_idx = None;
+    for i in (0..end_idx).rev() {
+        if on_digits[i].is_some() {
+            start_idx = Some(i + 1);
+            break;
+        }
+    }
+    (start_idx.unwrap_or(0), end_idx)
 }
 
 #[test]
 fn test_solve_line() {
-    assert_eq!(solve_line("12394565"), 96);
+    assert_eq!(solve_line("12394565", 2), 96);
 }
 
 #[test]
 fn test_solve_line_2() {
-    // assert_eq!(solve_line_2("987654321111111"), 1);
-    assert_eq!(solve_line_2("811111111111119"), 811111111119);
-    assert_eq!(solve_line_2("987654321111111"), 987654321111);
-    assert_eq!(solve_line_2("234234234234278"), 434234234278);
-    assert_eq!(solve_line_2("818181911112111"), 888911112111);
+    assert_eq!(solve_line("811111111111119", 12), 811111111119);
+    assert_eq!(solve_line("987654321111111", 12), 987654321111);
+    assert_eq!(solve_line("234234234234278", 12), 434234234278);
+    assert_eq!(solve_line("818181911112111", 12), 888911112111);
 }
 
 // 234234234234278 ->
