e46e3b5223b60daeba63c08d9034bf707d8a91a8
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-05 17:30:43 +0100

    add clap to parse args, continued

--- advent-of-code/2025/aocrust25/src/y2023.rs
+++ advent-of-code/2025/aocrust25/src/y2023.rs
@@ -1,8 +1,15 @@
 mod day2;
 
-pub fn run(_day: Option<i8>) {
-    println!("== 2023 == ");
-    day2::run();
+pub fn run(day: Option<i8>) {
+    if let Some(day) = day {
+        println!("== 2023 == ");
+        match day {
+            2 => day2::run(),
+            _ => unimplemented!(),
+        }
+    } else {
+        run_all();
+    }
 }
 
 pub fn run_all() {
