0c83608a9688ac678f6863f4660ce9bf9166bacf
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-08 11:36:11 +0100

    aoc 2025 day 6 p1 p2

--- advent-of-code/2025/aocrust25/src/utils/fs.rs
+++ advent-of-code/2025/aocrust25/src/utils/fs.rs
@@ -4,3 +4,8 @@ pub fn read(day: i8) -> std::io::Result<String> {
     let path = PathBuf::from(format!("./data/{day}.txt"));
     std::fs::read_to_string(path)
 }
+
+pub fn read_day_year(day: i8, year: i64) -> std::io::Result<String> {
+    let path = PathBuf::from(format!("./data/year/{year}/{day}.txt"));
+    std::fs::read_to_string(path)
+}
--- advent-of-code/2025/aocrust25/src/y2025/day6.rs
+++ advent-of-code/2025/aocrust25/src/y2025/day6.rs
@@ -1,12 +1,103 @@
-use crate::utils::fs::read;
-use std::ops::{Add, Mul};
+use crate::utils::fs::read_day_year;
+
+// p2 4451521054465 too low
+// p2 4456893221984 too low
 
 pub fn run() {
     println!("Day 6");
-    let inp = read(6).unwrap();
-    let inp = parse(&inp);
-    let p1 = solve(inp);
-    println!("p1 {p1}");
+    let inp = read_day_year(6, 2025).unwrap();
+    // let p1 = solve(parse(&inp));
+    // println!("p1 {p1}");
+    let p2 = solve2(&inp);
+    println!("p2 {p2}");
+}
+
+fn get_ops_w_widths(ops: &str) -> Vec<(usize, usize, Op)> {
+    let mut ret = vec![];
+
+    let ops: Vec<_> = ops.chars().collect();
+
+    let mut cur_op = ops[0];
+    let mut cur_width: usize = 1;
+    let mut cur_op_idx = 0;
+
+    for (idx, c) in ops.into_iter().enumerate().skip(1) {
+        if c == ' ' {
+            cur_width += 1;
+        } else {
+            ret.push((cur_width, cur_op_idx, cur_op));
+            cur_width = 1;
+            cur_op = c;
+            cur_op_idx = idx;
+        }
+    }
+
+    ret.push((cur_width, cur_op_idx, cur_op));
+
+    ret.iter()
+        .map(|&(width, idx, op)| {
+            let op = match op {
+                '*' => Op::Mul,
+                '+' => Op::Add,
+                _ => unreachable!(),
+            };
+            (width, idx, op)
+        })
+        .collect()
+}
+
+fn solve2(inp: &str) -> i64 {
+    let lines = inp.lines().collect::<Vec<&str>>();
+    let (&ops, nums) = lines.split_last().unwrap();
+    let grid_width = nums[0].len();
+
+    let ops_w_widths = get_ops_w_widths(ops);
+
+    let num_height = nums.len();
+    let nums = nums
+        .iter()
+        .flat_map(|line| line.chars())
+        .collect::<Vec<_>>();
+    // let ops = ops
+    //     .chars()
+    //     .filter_map(|c| match c {
+    //         ' ' => None,
+    //         '*' => Some(Op::Mul),
+    //         '+' => Some(Op::Add),
+    //         _ => unreachable!(),
+    //     })
+    //     .collect::<Vec<_>>();
+
+    ops_w_widths
+        .iter()
+        .map(|&(op_width, op_idx, op)| {
+            let eq_args: Vec<i64> = (0..(nums.len()))
+                .skip(op_idx)
+                .take(op_width)
+                .filter_map(|n_idx| {
+                    let str = (n_idx..nums.len())
+                        .step_by(grid_width)
+                        .take(num_height)
+                        .map(|idx| nums[idx])
+                        .collect::<String>();
+                    dbg!(n_idx, &str);
+                    let str = str.trim();
+                    match str.is_empty() {
+                        true => None,
+                        false => Some(str.parse::<i64>().unwrap()),
+                    }
+                })
+                .collect();
+
+            eq_args
+                .into_iter()
+                .reduce(|acc, n| match op {
+                    Op::Add => acc + n,
+                    Op::Mul => acc * n,
+                })
+                .unwrap()
+        })
+        .sum()
 }
 
 fn solve(inp: Input) -> i64 {
@@ -53,7 +144,7 @@ fn parse(inp: &str) -> Input {
         .skip(1)
         .map(|l| {
             l.split_ascii_whitespace()
-                .map(|n| dbg!(n).parse().unwrap())
+                .map(|n| n.parse().unwrap())
                 .collect()
         })
         .collect();
@@ -71,11 +162,11 @@ fn parse(inp: &str) -> Input {
 }
 
 #[test]
-fn test_parse() {
+fn test_2() {
     let inp = r"123 328  51 64 
  45 64  387 23 
   6 98  215 314
 *   +   *   +  ";
-    dbg!(parse(inp));
+    dbg!(solve2(inp));
     assert_eq!(0, 1);
 }
