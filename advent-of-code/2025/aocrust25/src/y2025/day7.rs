5c34bf35cdb0e2bb17bca4f63d75d72fdc55a2d9
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-09 12:05:07 +0100

    aoc 2025 day 7 refactor

--- advent-of-code/2025/aocrust25/src/y2025/day7.rs
+++ advent-of-code/2025/aocrust25/src/y2025/day7.rs
@@ -1,46 +1,18 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::utils::fs::read_day_year;
-// p2 380338654075 too low
 
 pub fn run() {
     println!("Day 7");
     let inp = read_day_year(7, 2025).unwrap();
-    let p1 = solve1(&inp);
+    let inp = process(&inp);
+    let p1 = inp.0;
+    let p2: i64 = inp.1.values().sum();
     println!("p1 {p1}");
-    let p2 = solve2(&inp);
     println!("p2 {p2}");
 }
 
-fn solve1(inp: &str) -> i64 {
-    inp.lines()
-        .enumerate()
-        .fold((0, HashSet::new()), |(splits, beams), (idx, line)| {
-            if idx == 0 {
-                let beam = line.chars().position(|c| c == 'S').unwrap();
-                let mut beams = HashSet::new();
-                beams.insert(beam);
-                (0, beams)
-            } else {
-                let mut next_beams = HashSet::new();
-                let mut next_splits = splits;
-                let chars: Vec<_> = line.chars().collect();
-                for &beam in beams.iter() {
-                    if chars[beam] == '^' {
-                        next_splits += 1;
-                        next_beams.insert(beam - 1);
-                        next_beams.insert(beam + 1);
-                    } else {
-                        next_beams.insert(beam);
-                    }
-                }
-                (next_splits, next_beams)
-            }
-        })
-        .0
-}
-
-fn solve2(inp: &str) -> i64 {
+fn process(inp: &str) -> (i64, std::collections::HashMap<usize, i64>) {
     inp.lines()
         .enumerate()
         .fold((0, HashMap::new()), |(splits, beams), (idx, line)| {
@@ -54,12 +26,6 @@ fn solve2(inp: &str) -> i64 {
                 let mut next_splits = splits;
                 let chars: Vec<_> = line.chars().collect();
                 for (&beam, &count) in beams.iter() {
-                    let should_dbg = idx == 10 && beam == 9;
-
-                    if should_dbg {
-                        dbg!(idx, beam, count, next_beams.get(&(beam - 1)));
-                    }
-
                     if chars[beam] == '^' {
                         next_splits += 1;
 
@@ -69,13 +35,9 @@ fn solve2(inp: &str) -> i64 {
                         *next_beams.entry(beam).or_insert(0) += count;
                     }
                 }
-                dbg!(idx, &next_beams, &next_beams.values().sum::<i64>());
                 (next_splits, next_beams)
             }
         })
-        .1
-        .values()
-        .sum()
 }
 
 #[test]
@@ -96,5 +58,5 @@ fn test_2() {
 ...............
 .^.^.^.^.^...^.
 ...............";
-    assert_eq!(solve2(inp), 40);
+    assert_eq!(process(inp).1.values().sum::<i64>(), 40);
 }
