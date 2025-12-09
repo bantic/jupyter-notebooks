ba513b088cc0e48e5287886b449e9125f3442ed4
Cory Forsyth <cory.forsyth@addepar.com>
2025-12-09 12:15:50 +0100

    aoc 2025 day 7 refactor

--- advent-of-code/2025/aocrust25/src/y2025/day7.rs
+++ advent-of-code/2025/aocrust25/src/y2025/day7.rs
@@ -5,39 +5,39 @@ use crate::utils::fs::read_day_year;
 pub fn run() {
     println!("Day 7");
     let inp = read_day_year(7, 2025).unwrap();
-    let inp = process(&inp);
-    let p1 = inp.0;
-    let p2: i64 = inp.1.values().sum();
+    let (p1, p2) = solve(&inp);
     println!("p1 {p1}");
     println!("p2 {p2}");
 }
 
-fn process(inp: &str) -> (i64, std::collections::HashMap<usize, i64>) {
-    inp.lines()
-        .enumerate()
-        .fold((0, HashMap::new()), |(splits, beams), (idx, line)| {
-            if idx == 0 {
-                let beam = line.chars().position(|c| c == 'S').unwrap();
-                let mut beams = HashMap::new();
-                beams.insert(beam, 1);
-                (0, beams)
-            } else {
-                let mut next_beams = HashMap::new();
-                let mut next_splits = splits;
-                let chars: Vec<_> = line.chars().collect();
-                for (&beam, &count) in beams.iter() {
-                    if chars[beam] == '^' {
-                        next_splits += 1;
+fn solve(inp: &str) -> (i64, i64) {
+    let lines = inp.lines().collect::<Vec<_>>();
+    let (first, rest) = lines.split_first().unwrap();
 
-                        *next_beams.entry(beam - 1).or_insert(0) += count;
-                        *next_beams.entry(beam + 1).or_insert(0) += count;
-                    } else {
-                        *next_beams.entry(beam).or_insert(0) += count;
-                    }
-                }
-                (next_splits, next_beams)
+    let init_beam = first.chars().position(|c| c == 'S').unwrap();
+    let mut init_beams = HashMap::new();
+    init_beams.insert(init_beam, 1);
+    let init_splits = 0;
+    let init_state = (init_splits, init_beams);
+
+    let processed = rest.iter().fold(init_state, |(splits, beams), line| {
+        let mut next_beams = HashMap::new();
+        let mut next_splits = splits;
+        let chars: Vec<_> = line.chars().collect();
+        for (&beam, &count) in beams.iter() {
+            if chars[beam] == '^' {
+                next_splits += 1;
+
+                *next_beams.entry(beam - 1).or_insert(0) += count;
+                *next_beams.entry(beam + 1).or_insert(0) += count;
+            } else {
+                *next_beams.entry(beam).or_insert(0) += count;
             }
-        })
+        }
+        (next_splits, next_beams)
+    });
+
+    (processed.0, processed.1.values().sum())
 }
 
 #[test]
@@ -58,5 +58,6 @@ fn test_2() {
 ...............
 .^.^.^.^.^...^.
 ...............";
-    assert_eq!(process(inp).1.values().sum::<i64>(), 40);
+    assert_eq!(solve(inp).0, 21);
+    assert_eq!(solve(inp).1, 40);
 }
